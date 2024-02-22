86609de58c43968ae3b00af15a2f1d0d99ec5d8d
Ferdinand Saurenbach <ferdinand161@outlook.de>
2024-02-22 15:05:51 +0100

    Update

--- src/commonMain/kotlin/Piece.kt
+++ src/commonMain/kotlin/Piece.kt
@@ -3,11 +3,9 @@ import korlibs.image.color.RGBA
 import korlibs.korge.input.*
 import korlibs.korge.render.RenderContext
 import korlibs.korge.scene.SceneContainer
-import korlibs.korge.view.Image
-import korlibs.korge.view.View
-import korlibs.korge.view.addTo
-import korlibs.korge.view.size
+import korlibs.korge.view.*
 import korlibs.math.geom.*
+import kotlinx.coroutines.*
 
 class Piece(kind: PieceKind, color: RGBA, i: Int, j: Int, cont: SceneContainer) : View() {
 
@@ -16,34 +14,39 @@ class Piece(kind: PieceKind, color: RGBA, i: Int, j: Int, cont: SceneContainer) 
         var position = board[i][j].pos
         init {
             if (color == Colors.WHITE) {
-                if (kind == PieceKind.Pawn) {
+                if (kind == PieceKind.whitePawn) {
                     piece.size(Size(64, 64))
                     piece.addTo(cont) // Add the piece to the scene first
                     moveTo(i, j) // Then update its position
                 }
             }
-            clickListener()
+            clickListener(i, j)
 
         }
 
-    fun moveTo(i: Int, j: Int) {
+    private fun moveTo(i: Int, j: Int) {
         // Update the position of 'piece' based on 'i' and 'j'
-        piece.pos = position
+        piece.pos = board[i][j].pos
     }
-    fun decodePosition(i: Point){
-        var x = i.x /64
-        var y = i.y/64
-        return board[x][y]
-
+    private fun decodePosition(i: Point): Pair<Int, Int> {
+        val x = i.x / 64
+        val y = i.y / 64
+        val yx = "[$y][$x]"
+        return Pair(y.toInt(), x.toInt())
     }
 
 
-    fun clickListener(){
+
+    private fun clickListener(i: Int, j: Int){
         piece.onClick {
             println(pieceKind)
             println("position")
             println(position)
+            println("pair")
             println(decodePosition(position))
+            lastClicked = board[i][j].pos
+            println(decodePosition(lastClicked!!))
+
 
         }
     }
--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -10,6 +10,7 @@ import korlibs.math.geom.*
 
 val board = Array(8) { Array(8) { SolidRect(512 / 8, 512 / 8) } }
 var whitePawn: Bitmap? = null
+var lastClicked: Point? = null
 suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors["#2b2b2b"]) {
 	val sceneContainer = sceneContainer()
 
@@ -17,15 +18,13 @@ suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors
 	sceneContainer.changeTo{ GameScene(sceneContainer) }
 }
 enum class PieceKind(kind: String){
-    Pawn("P"),
-    Bishop("B"),
-    Knight("K"),
-    Rook("R"),
-    Queen("Q"),
-    King("K");
-
-
-
+    whitePawn("P"),
+    blackPawn("P")
+    //Bishop("B"),
+    //Knight("K"),
+    //Rook("R"),
+    //Queen("Q"),
+    //King("K");
 }
 
 
@@ -48,7 +47,7 @@ class GameScene(cont: SceneContainer) : PixelatedScene(512, 512) {
             d++
         }
 
-        var p = Piece(PieceKind.Pawn, Colors.WHITE, 0, 0, cont = sceneContainer)
+        var p = Piece(PieceKind.whitePawn, Colors.WHITE, 5, 3, cont = sceneContainer)
 
     }
 }
