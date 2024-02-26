8d96e2ebc67a6146f5de26994392f01f77290e5a
Ferdinand Saurenbach <ferdinand161@outlook.de>
2024-02-26 18:24:33 +0100

    Delete build directory

--- build/platforms/android/androisrc/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.sample.demo
-import korlibs.render.*
-import main.main
-class MainActivity : KorgwActivity(config = GameWindowCreationConfig(msaa = 1, fullscreen = null)) {
-	override suspend fun activityMain() {
-		main.main()
-	}
-}
