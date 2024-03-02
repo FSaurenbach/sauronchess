632345076422d8ec58b4b5bc018e8e3b18a4a28c
Ferdinand Saurenbach <ferdinand161@outlook.de>
2024-03-02 10:53:54 +0100

    #5

--- src/commonMain/kotlin/Piece.kt
+++ src/commonMain/kotlin/Piece.kt
@@ -17,8 +17,23 @@ fun decodePosition(cxy: Point): Pair<Int, Int> {
   return Pair(x.toInt(), y.toInt())
 }
 
-class Piece(kind: PieceKind, private val color: RGBA, cx: Int, cy: Int, cont: SceneContainer) :
-  View() {
+/**
+ * Represents a chess piece.
+ *
+ * @constructor Creates a Piece with the specified parameters.
+ * @property kind The kind of piece.
+ * @property color The color of the piece.
+ * @property cx The x-coordinate of the piece.
+ * @property cy The y-coordinate of the piece.
+ * @property cont The scene container.
+ */
+class Piece(
+  kind: PieceKind,
+  private val color: RGBA,
+  private val cx: Int,
+  private val cy: Int,
+  private val cont: SceneContainer,
+) : View() {
 
   var pieceKind: PieceKind = kind
   private lateinit var piece: Image
@@ -26,51 +41,46 @@ class Piece(kind: PieceKind, private val color: RGBA, cx: Int, cy: Int, cont: Sc
 
   init {
     if (color == Colors.WHITE) {
-      if (kind == PieceKind.WhitePawn) {
-        piece = Image(whitePawn!!)
+      if (kind == PieceKind.WhitePawn || kind == PieceKind.WhiteRook) {
+        piece = Image(if (kind == PieceKind.WhitePawn) whitePawn!! else whiteRook!!)
         piece.size(Size(64, 64))
-        piece.addTo(cont) // Add the piece to the scene first
-        moveTo(cx, cy) // Then update its position
+        piece.addTo(cont)
+        moveTo(cx, cy)
       }
-      if (kind == PieceKind.WhiteRook) {
-        piece = Image(whiteRook!!)
+    } else {
+      if (kind == PieceKind.BlackPawn || kind == PieceKind.BlackRook) {
+        piece = Image(if (kind == PieceKind.BlackPawn) blackPawn!! else blackRook!!)
         piece.size(Size(64, 64))
-        piece.addTo(cont) // Add the piece to the scene first
-        moveTo(cx, cy) // Then update its position
-      }
-    }
-    if (color == Colors.BLACK) {
-      if (kind == PieceKind.BlackPawn) {
-        piece = Image(blackPawn!!)
-        piece.size(Size(64, 64))
-        piece.addTo(cont) // Add the piece to the scene first
-        moveTo(cx, cy) // Then update its position
-      }
-      if (kind == PieceKind.BlackRook) {
-        piece = Image(blackRook!!)
-        piece.size(Size(64, 64))
-        piece.addTo(cont) // Add the piece to the scene first
-        moveTo(cx, cy) // Then update its position
+        piece.addTo(cont)
+        moveTo(cx, cy)
       }
     }
-
-    // clickListener(cx, cy)
-
   }
 
+  /**
+   * Moves the piece to a new position on the board.
+   *
+   * @param cx The new x-coordinate of the piece.
+   * @param cy The new y-coordinate of the piece.
+   */
   fun moveTo(cx: Int, cy: Int) {
-    // Update the position of 'piece' based on 'cx' and 'cy'
     piece.position((cx * 64), (cy * 64))
     position = board[cy][cx].pos
   }
 
+  /**
+   * Checks if the move of the piece is valid.
+   *
+   * @param oldPos The old position of the piece.
+   * @param newPos The new position of the piece.
+   * @param withCheck Indicates whether to perform the move or just check its validity.
+   * @return true if the move is valid, false otherwise.
+   */
   fun moveChecker(oldPos: Pair<Int, Int>, newPos: Pair<Int, Int>, withCheck: Boolean): Boolean {
-    val kind = this.pieceKind
-
     val pieceOnNewPos = pieces.find { it.position == board[newPos.second][newPos.first].pos }
 
     if (whiteTurn) {
-      when (kind) {
+      when (pieceKind) {
         PieceKind.WhitePawn -> {
           if (
             (newPos.second - oldPos.second == 1 && oldPos.first == newPos.first) ||
@@ -80,14 +90,10 @@ class Piece(kind: PieceKind, private val color: RGBA, cx: Int, cy: Int, cont: Sc
               if (withCheck) whiteTurn = false
               return true
             }
-          }
-          // Capture a piece
-          else if (
+          } else if (
             newPos.second - oldPos.second == 1 &&
               ((newPos.first - oldPos.first == 1) || (newPos.first - oldPos.first == -1))
           ) {
-
-            println("Destiny: $pieceOnNewPos")
             if (pieceOnNewPos != null && pieceOnNewPos.pieceKind == PieceKind.BlackPawn) {
               if (withCheck) {
                 pieces.remove(pieceOnNewPos)
@@ -100,10 +106,9 @@ class Piece(kind: PieceKind, private val color: RGBA, cx: Int, cy: Int, cont: Sc
         }
         PieceKind.WhiteRook -> {
           if (oldPos.first != newPos.first && oldPos.second != newPos.second) {
-            return false // Rook can only move along rows or columns
+            return false
           }
 
-          // Check for any pieces between oldPos and newPos
           val minRow = minOf(oldPos.first, newPos.first)
           val maxRow = maxOf(oldPos.first, newPos.first)
           val minCol = minOf(oldPos.second, newPos.second)
@@ -112,15 +117,14 @@ class Piece(kind: PieceKind, private val color: RGBA, cx: Int, cy: Int, cont: Sc
           for (piece in pieces) {
             val row = decodePosition(piece.position).first
             val col = decodePosition(piece.position).second
-            if (row == oldPos.first && col == oldPos.second)
-              continue // Skip the rook's own position
-            if (row == newPos.first && col == newPos.second) continue // Skip the target position
+            if (row == oldPos.first && col == oldPos.second) continue
+            if (row == newPos.first && col == newPos.second) continue
             if (
               (row == oldPos.first || col == oldPos.second) &&
                 row in minRow..maxRow &&
                 col in minCol..maxCol
             ) {
-              return false // There's a piece blocking the path
+              return false
             }
           }
 
@@ -131,17 +135,15 @@ class Piece(kind: PieceKind, private val color: RGBA, cx: Int, cy: Int, cont: Sc
             return true
           }
           if (pieceOnNewPos == null) {
-
-            return true // Rook can move without obstruction
+            return true
           }
         }
         else -> {
           return false
         }
       }
-    }
-    if (!whiteTurn) {
-      when (kind) {
+    } else {
+      when (pieceKind) {
         PieceKind.BlackPawn -> {
           if (
             (newPos.second - oldPos.second == -1 && oldPos.first == newPos.first) ||
@@ -152,9 +154,7 @@ class Piece(kind: PieceKind, private val color: RGBA, cx: Int, cy: Int, cont: Sc
 
               return true
             }
-          }
-          // If the pawn is trying to capture a piece
-          else if (
+          } else if (
             newPos.second - oldPos.second == -1 &&
               ((newPos.first - oldPos.first == 1) || (newPos.first - oldPos.first == -1))
           ) {
@@ -171,10 +171,9 @@ class Piece(kind: PieceKind, private val color: RGBA, cx: Int, cy: Int, cont: Sc
         }
         PieceKind.BlackRook -> {
           if (oldPos.first != newPos.first && oldPos.second != newPos.second) {
-            return false // Rook can only move along rows or columns
+            return false
           }
 
-          // Check for any pieces between oldPos and newPos
           val minRow = minOf(oldPos.first, newPos.first)
           val maxRow = maxOf(oldPos.first, newPos.first)
           val minCol = minOf(oldPos.second, newPos.second)
@@ -183,15 +182,14 @@ class Piece(kind: PieceKind, private val color: RGBA, cx: Int, cy: Int, cont: Sc
           for (piece in pieces) {
             val row = decodePosition(piece.position).first
             val col = decodePosition(piece.position).second
-            if (row == oldPos.first && col == oldPos.second)
-              continue // Skip the rook's own position
-            if (row == newPos.first && col == newPos.second) continue // Skip the target position
+            if (row == oldPos.first && col == oldPos.second) continue
+            if (row == newPos.first && col == newPos.second) continue
             if (
               (row == oldPos.first || col == oldPos.second) &&
                 row in minRow..maxRow &&
                 col in minCol..maxCol
             ) {
-              return false // There's a piece blocking the path
+              return false
             }
           }
           if (withCheck && pieceOnNewPos != null && pieceOnNewPos.color == Colors.WHITE) {
@@ -201,7 +199,7 @@ class Piece(kind: PieceKind, private val color: RGBA, cx: Int, cy: Int, cont: Sc
             return true
           }
           if (pieceOnNewPos == null) {
-            return true // Rook can move without obstruction
+            return true
           }
         }
         else -> {
