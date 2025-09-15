b6cfa5d1efcb89dd6b86b96b1e9d6b2169b25c45
Ferdinand Saurenbach <ferdinand161@outlook.de>
2025-09-15 17:51:27 +0200

    project: remove unused kproject

--- src/Helpers.kt
+++ src/Helpers.kt
@@ -7,32 +7,32 @@ fun removePiece(piece: Piece) {
     pieces.remove(piece)
     piece.removeFromParent()
 }
-fun addAllPieces(cont: Container) {
+fun addAllPieces(chessboard: Container) {
     // Add all pieces in right order and add them to the pieces list (white pieces are at the
     // bottom)
     for (i in 0 until 8) {
-        cont.piece(PieceKind.WhitePawn, Colors.WHITE, i, 6, isWhite = true)
+        chessboard.piece(PieceKind.WhitePawn, Colors.WHITE, i, 6, isWhite = true)
     }
-    cont.piece(PieceKind.WhiteRook, Colors.WHITE, 0, 7, isWhite = true)
-    cont.piece(PieceKind.WhiteRook, Colors.WHITE, 7, 7, isWhite = true)
-    cont.piece(PieceKind.WhiteKnight, Colors.WHITE, 1, 7, isWhite = true)
-    cont.piece(PieceKind.WhiteKnight, Colors.WHITE, 6, 7, isWhite = true)
-    cont.piece(PieceKind.WhiteBishop, Colors.WHITE, 2, 7, isWhite = true)
-    cont.piece(PieceKind.WhiteBishop, Colors.WHITE, 5, 7, isWhite = true)
-    cont.piece(PieceKind.WhiteQueen, Colors.WHITE, 3, 7, isWhite = true)
-    cont.piece(PieceKind.WhiteKing, Colors.WHITE, 4, 7, isWhite = true)
+    chessboard.piece(PieceKind.WhiteRook, Colors.WHITE, 0, 7, isWhite = true)
+    chessboard.piece(PieceKind.WhiteRook, Colors.WHITE, 7, 7, isWhite = true)
+    chessboard.piece(PieceKind.WhiteKnight, Colors.WHITE, 1, 7, isWhite = true)
+    chessboard.piece(PieceKind.WhiteKnight, Colors.WHITE, 6, 7, isWhite = true)
+    chessboard.piece(PieceKind.WhiteBishop, Colors.WHITE, 2, 7, isWhite = true)
+    chessboard.piece(PieceKind.WhiteBishop, Colors.WHITE, 5, 7, isWhite = true)
+    chessboard.piece(PieceKind.WhiteQueen, Colors.WHITE, 3, 7, isWhite = true)
+    chessboard.piece(PieceKind.WhiteKing, Colors.WHITE, 4, 7, isWhite = true)
 
     for (i in 0 until 8) {
-        cont.piece(PieceKind.BlackPawn, Colors.BLACK, i, 1, isWhite = false)
+        chessboard.piece(PieceKind.BlackPawn, Colors.BLACK, i, 1, isWhite = false)
     }
-    cont.piece(PieceKind.BlackRook, Colors.BLACK, 0, 0, isWhite = false)
-    cont.piece(PieceKind.BlackRook, Colors.BLACK, 7, 0, isWhite = false)
-    cont.piece(PieceKind.BlackKnight, Colors.BLACK, 1, 0, isWhite = false)
-    cont.piece(PieceKind.BlackKnight, Colors.BLACK, 6, 0, isWhite = false)
-    cont.piece(PieceKind.BlackBishop, Colors.BLACK, 2, 0, isWhite = false)
-    cont.piece(PieceKind.BlackBishop, Colors.BLACK, 5, 0, isWhite = false)
-    cont.piece(PieceKind.BlackQueen, Colors.BLACK, 3, 0, isWhite = false)
-    cont.piece(PieceKind.BlackKing, Colors.BLACK, 4, 0, isWhite = false)
+    chessboard.piece(PieceKind.BlackRook, Colors.BLACK, 0, 0, isWhite = false)
+    chessboard.piece(PieceKind.BlackRook, Colors.BLACK, 7, 0, isWhite = false)
+    chessboard.piece(PieceKind.BlackKnight, Colors.BLACK, 1, 0, isWhite = false)
+    chessboard.piece(PieceKind.BlackKnight, Colors.BLACK, 6, 0, isWhite = false)
+    chessboard.piece(PieceKind.BlackBishop, Colors.BLACK, 2, 0, isWhite = false)
+    chessboard.piece(PieceKind.BlackBishop, Colors.BLACK, 5, 0, isWhite = false)
+    chessboard.piece(PieceKind.BlackQueen, Colors.BLACK, 3, 0, isWhite = false)
+    chessboard.piece(PieceKind.BlackKing, Colors.BLACK, 4, 0, isWhite = false)
 }
 
 /**Load bitmaps*/
